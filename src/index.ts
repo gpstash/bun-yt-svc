1d9711dbc26a540c87d1e088210b886951d6163a
Gilang Pratama Putra <putra@gilangpratama.id>
2025-08-14 08:48:20 +0700

    feat: add port configuration and export fetch handler for Hono app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono!');
 })
 
-export default app
+export default {
+  fetch: app.fetch,
+  port: Number(process.env.APP_PORT) ?? 1331,
+};
